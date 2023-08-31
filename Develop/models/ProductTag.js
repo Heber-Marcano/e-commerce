c617c5fa884fab40f83f28b225c9b286288ea300
Heber Marcano <Hebelioo@gmail.com>
2023-08-30 20:48:06 -0400

    added product tags

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,11 +1,33 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        // This references the `reader` model, which we set in `Reader.js` as its `modelName` property
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        // This references the `reader` model, which we set in `Reader.js` as its `modelName` property
+        model: 'tag',
+        key: 'id',
+      },
+    }
     // define columns
   },
   {
@@ -13,7 +35,7 @@ ProductTag.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
